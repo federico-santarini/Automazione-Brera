44493f0309a8a7b74ef3abbe2d110a4a0630e9a1
federico-santarini <federico.santarini@gmail.com>
2022-04-14 10:10:47 +0200

    Updated file system

--- source/sharedValues.py
+++ source/sharedValues.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-PROJECT_FOLDER = "/Users/federicosantarini/CloudStation/_ Lavori/Automazione Brera/project_folder"
+PROJECT_FOLDER = "/Users/federico/CloudStation/Federico/_ Lavori/Automazione Brera/project_folder"
 
 JSON_PATH = PROJECT_FOLDER+'/breradesignweek_2019_events_20200122.json'
 
