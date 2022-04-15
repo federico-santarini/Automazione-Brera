c11dd4c6b90ad8748baeb7fb6a9988e159a7d023
federico-santarini <federico.santarini@gmail.com>
2022-04-15 14:21:14 +0200

    added JSON_FILE field

--- source/sharedValues.py
+++ source/sharedValues.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 PROJECT_FOLDER = "/Users/federico/CloudStation/Federico/_ Lavori/Automazione Brera/project_folder"
-
-JSON_PATH = PROJECT_FOLDER+'/breradesignweek_2019_events_20200122.json'
+JSON_FILE = 'breradesignweek_2019_events_20200122.json'
+JSON_PATH = PROJECT_FOLDER+'/'+JSON_FILE
 
 EXCEPTIONS_CSV = 'exceptions.csv'
